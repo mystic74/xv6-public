56b4df53ee1376aa3abb55fe58e3c5c9e0efbe18
os202 <brander@post.bgu.ac.il>
2020-04-05 16:42:51 +0300

    rachel git test

--- proc.h
+++ proc.h
@@ -61,6 +61,7 @@ struct proc
   struct inode *cwd;          // Current directory
   char name[16];              // Process name (debugging)
   int status;
+  /*test for git/*
 };
 
 // Process memory is laid out contiguously, low addresses first:
