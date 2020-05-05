1cc66e6316ad5b2336a486ec05b96290c6fd12eb
Tom Ronen <mystic74@gmail.com>
2020-05-06 00:09:00 +0300

    kill : changing it with default
    Adding default value, now checking to see if we get
    two or one parameter

--- kill.c
+++ kill.c
@@ -5,13 +5,21 @@
 int
 main(int argc, char **argv)
 {
-  int i;
-
-  if(argc < 3){
-    printf(2, "usage: kill pid...\n");
+  if((argc > 3) || (argc == 1)) {
+    printf(2, "usage: kill <signal> pid...\n defualt signal is SIGKILL \n");
     exit();
   }
-  for(i=1; i<argc-1; i+=2)
-    kill(atoi(argv[i]),atoi(argv[i+1]));
+  if (argc == 2)
+  {
+    printf(1, "sending to %x \n", atoi(argv[1]));
+
+    kill(atoi(argv[1]),SIGKILL);
+  }
+  else
+  {
+    printf(1, "sending %x \n", atoi(argv[1]));
+    printf(1, "to %x \n", atoi(argv[2]));
+    kill(atoi(argv[2]),atoi(argv[1]));
+  } 
   exit();
 }
