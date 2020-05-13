e96e9f6eaab6bf85729c96c570296bfe6d3866e5
Tom Ronen <mystic74@gmail.com>
2020-05-13 11:09:40 +0300

    usertests : fixed
    Now with real kill

--- usertests.c
+++ usertests.c
@@ -8,7 +8,6 @@
 #include "traps.h"
 #include "memlayout.h"
 
-
 char buf[8192];
 char name[3];
 char *echoargv[] = { "echo", "ALL", "TESTS", "PASSED", 0 };
@@ -391,10 +390,9 @@ preempt(void)
   }
   close(pfds[0]);
   printf(1, "kill... ");
-  
-  /*kill(pid1,SIGKILL);
-  kill(pid2,SIGKILL);
-  kill(pid3,SIGKILL);*/
+  kill(pid1, SIGKILL);
+  kill(pid2, SIGKILL);
+  kill(pid3, SIGKILL);
   printf(1, "wait... ");
   wait();
   wait();
@@ -430,10 +428,10 @@ void
 mem(void)
 {
   void *m1, *m2;
-  int pid; //ppid;
+  int pid, ppid;
 
   printf(1, "mem test\n");
-  //ppid = getpid();
+  ppid = getpid();
   if((pid = fork()) == 0){
     m1 = 0;
     while((m2 = malloc(10001)) != 0){
@@ -448,7 +446,7 @@ mem(void)
     m1 = malloc(1024*20);
     if(m1 == 0){
       printf(1, "couldn't allocate mem?!!\n");
-      //kill(ppid,SIGKILL);
+      kill(ppid, SIGKILL);
       exit();
     }
     free(m1);
@@ -1416,7 +1414,7 @@ forktest(void)
 void
 sbrktest(void)
 {
-  int fds[2], pid, pids[10];// ppid;
+  int fds[2], pid, pids[10], ppid;
   char *a, *b, *c, *lastaddr, *oldbrk, *p, scratch;
   uint amt;
 
@@ -1497,7 +1495,7 @@ sbrktest(void)
 
   // can we read the kernel's memory?
   for(a = (char*)(KERNBASE); a < (char*) (KERNBASE+2000000); a += 50000){
-    //ppid = getpid();
+    ppid = getpid();
     pid = fork();
     if(pid < 0){
       printf(stdout, "fork failed\n");
@@ -1505,7 +1503,7 @@ sbrktest(void)
     }
     if(pid == 0){
       printf(stdout, "oops could read %x = %x\n", a, *a);
-     // kill(ppid),SIGKILL;
+      kill(ppid, SIGKILL);
       exit();
     }
     wait();
@@ -1534,7 +1532,7 @@ sbrktest(void)
   for(i = 0; i < sizeof(pids)/sizeof(pids[0]); i++){
     if(pids[i] == -1)
       continue;
-    //kill(pids[i],SIGKILL);
+    kill(pids[i], SIGKILL);
     wait();
   }
   if(c == (char*)0xffffffff){
@@ -1578,7 +1576,7 @@ validatetest(void)
     }
     sleep(0);
     sleep(0);
-    //kill(pid,SIGKILL);
+    kill(pid, SIGKILL);
     wait();
 
     // try to crash the kernel by passing in a bad string pointer
