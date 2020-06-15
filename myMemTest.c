2baa4b1d00a9713c4ed6a9b6cdc9ef407ac12731
Tom Ronen <mystic74@gmail.com>
2020-06-15 14:25:51 +0300

    uniting memtests

--- myMemTest.c
+++ myMemTest.c
@@ -2,9 +2,106 @@
 #include "stat.h"
 #include "user.h"
 #include "fs.h"
+#include "param.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "memlayout.h"
+#include "mmu.h"
 
 #define PGSIZE 4096
 #define ARR_SIZE 82000
+#define N_PAGES 24
+
+char *data[N_PAGES];
+
+void bigTest()
+{
+    int i = 0;
+    int n = N_PAGES;
+
+    for (i = 0; i < n;)
+    {
+        data[i] = sbrk(PGSIZE);
+        data[i][0] = 00 + i;
+        data[i][1] = 10 + i;
+        data[i][2] = 20 + i;
+        data[i][3] = 30 + i;
+        data[i][4] = 40 + i;
+        data[i][5] = 50 + i;
+        data[i][6] = 60 + i;
+        data[i][7] = 70 + i;
+        printf(1, "allocated new page #%d at address: %x\n", i, data[i]);
+        i++;
+    }
+
+    printf(1, "\nIterate through pages seq:\n");
+
+    int j;
+    for (j = 1; j < n; j++)
+    {
+        printf(1, "j:  %d\n", j);
+
+        for (i = 0; i < j; i++)
+        {
+            data[i][10] = 2; // access to the i-th page
+            printf(1, "%d, ", i);
+        }
+        printf(1, "\n");
+    }
+
+    int k;
+    int pid = fork();
+    if (pid)
+        wait();
+    else
+    {
+        printf(1, "\nGo through same 8 pages and different 8 others\n");
+        for (j = 0; j < 8; j++)
+        {
+            for (i = 20; i < 24; i++)
+            {
+                data[i][10] = 1;
+                printf(1, "%d, ", i);
+            }
+            printf(1, "\n");
+            switch (j % 4)
+            {
+            case 0:
+                for (k = 0; k < 4; k++)
+                {
+                    data[k][10] = 1;
+                    printf(1, "%d, ", k);
+                }
+                break;
+            case 1:
+                for (k = 4; k < 8; k++)
+                {
+                    data[k][10] = 1;
+                    printf(1, "%d, ", k);
+                }
+                break;
+            case 2:
+                for (k = 8; k < 12; k++)
+                {
+                    data[k][10] = 1;
+                    printf(1, "%d, ", k);
+                }
+                break;
+            case 3:
+                for (k = 12; k < 16; k++)
+                {
+                    data[k][10] = 1;
+                    printf(1, "%d, ", k);
+                }
+                break;
+            }
+
+            // data[j][10] = 0;
+            // printf(1,"%d, ",j);
+            printf(1, "\n");
+        }
+    }
+}
 
 /*
 	Test used to check the swapping machanism in fork.
@@ -101,9 +198,11 @@ void globalTest()
 
 int main(int argc, char *argv[])
 {
-    printf(1, "Running global test\n");
-    globalTest(); //for testing each policy efficiency
+    // printf(1, "Running global test\n");
+    // globalTest(); //for testing each policy efficiency
     // printf(1, "Running mini fork test\n");
     // forkTest(); //for testing swapping machanism in fork.
+    printf(1, "running big test \n");
+    bigTest();
     exit();
 }
